86f5dc62ee6ac7cd2aa6bfa65a22330125e81dad
zhangxiu000402 <1740029706@qq.com>
2019-12-24 23:45:11 +0800

    zx

---
+++ nav.js
@@ -0,0 +1,9 @@
+var daohang = document.getElementById("daohang");
+var img = document.getElementById("img");
+daohang.onclick=function(){
+	document.getElementById("top").style.display='block';
+}
+img.onclick=function(){
+	document.getElementById("top").style.display='none';
+}
+
